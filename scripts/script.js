403b10a83f86c14eb72c68550a04246c67830bf2
Marcel <60448916+marcelmos@users.noreply.github.com>
2020-10-21 13:50:12 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function newsletterJoin(){
+    let inputField = document.getElementById("singin-newsletter");
+
+    inputField.value = null;
+}
+
+function contact(){
+    let inputName = document.getElementById("contact-name");
+    let inputMail = document.getElementById("contact-mail");
+
+    inputName.value = null;
+    inputMail.value = null;
+}
